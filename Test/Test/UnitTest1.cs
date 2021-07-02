0dae98b078601e12c00e16f6eff29a7d1add12f2
Eduardo Perez Leale <eduardo.perez@nextgames.com>
2021-07-02 23:36:27 +0300

    Remove comments. Rename function. Remove unused imports.

--- Test/Test/UnitTest1.cs
+++ Test/Test/UnitTest1.cs
@@ -1,39 +1,10 @@
-using System;
-using System.Collections.Generic;
 using Xunit;
 
 namespace Test
 {
     public class UnitTest1
     {
-        // Symbol    Value
-        // I            1
-        // V            5
-        // X            10
-        // L            50
-        // C            100
-        // D            500
-        // M            1000
-        
-        // - Generally, symbols are placed in order of value,
-        //     - Starting with the largest values
-        //     - When smaller values precede larger values, the smaller values are subtracted
-        //         from the larger values, and the result is added to the total
-        
-        // - A number written in Arabic numerals can be broken into digits.
-        // - For example, 1903 is composed of 1 (one thousand), 9 (nine hundreds), 0 (zero tens),
-        // and 3 (three units).
-        // - To write the Roman numeral, each of the non-zero digits should be treated separately.
-        // - In the above example, 1,000 = M, 900 = CM, and 3 = III. Therefore, 1903 = MCMIII.
-        // - The symbols I, X, C, and M can be repeated three times in succession, but no more.
-        // (They may appear more than three times if they appear non-sequentially, such as XXXIX)
-        //     D, L, and V. can never be repeated.
-        //     I can be subtracted from V and X only.
-        //     X can be subtracted from L and C only.
-        //     C can be subtracted from D and M only.
-        //     V, L, and D can never be subtracted.
-        //     Only one small-value symbol may be subtracted from any large-value symbol.
-        
+
         [Fact]
         public void ArabicToRoman_With_Number_1_Returns_I()
         {
@@ -173,13 +144,13 @@ namespace Test
         private string ArabicToRoman(int number)
         {
             int[] digits = SplitNumberIntoDigits(number);
-            return Convert(digits[0], new []{'M'}) + 
-                   Convert(digits[1], new char[] {'C', 'D', 'M'}) + 
-                   Convert(digits[2], new char[] {'X', 'L', 'C'}) + 
-                   Convert(digits[3], new char[] {'I', 'V', 'X'});
+            return GetRomanSymbolsForDigit(digits[0], new []{'M'}) + 
+                   GetRomanSymbolsForDigit(digits[1], new char[] {'C', 'D', 'M'}) + 
+                   GetRomanSymbolsForDigit(digits[2], new char[] {'X', 'L', 'C'}) + 
+                   GetRomanSymbolsForDigit(digits[3], new char[] {'I', 'V', 'X'});
         }
 
-        private static string Convert(int digit, char[]romanLetters)
+        private static string GetRomanSymbolsForDigit(int digit, char[]romanLetters)
         {
             if (digit >= 1 && digit <= 3)
                 return new string(romanLetters[0], digit);
